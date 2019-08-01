c23b983c352478f85049e7bf6ebce8a4575ca96a
codacy20 <kmuc72@gmail.com>
2019-08-01 17:16:05 +0200

    basic controller and model for expenses

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Query, Body, Post } from '@nestjs/common';
 import { CreateExpenseDto } from './dto/createExpense.dto';
+import { ExpenseService } from './expense.service'
+import { Expense } from './interfaces/expense.dto'
 
 @Controller('expense')
 export class ExpenseController {
+
+    constructor(private readonly expenseService: ExpenseService) { }
+
     @Get()
-    findAll() {
-        return `This action returns all expenses`;
+    async findAll(): Promise<Expense[]> {
+        return this.expenseService.findAll();
     }
 
     @Post()
-    create(@Body() createExpenseDto: CreateExpenseDto) {
-        return 'This action adds a new cat';
+    async create(@Body() createExpenseDto: CreateExpenseDto) {
+        this.expenseService.create(createExpenseDto);
     }
 }
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { Expense } from './interfaces/expense.dto'
 @Injectable()
-export class ExpenseService {}
+export class ExpenseService {
+    private readonly expenses: Expense[] = [];
+
+    create(expense: Expense) {
+        this.expenses.push(expense);
+    }
+
+    findAll(): Expense[] {
+        return this.expenses;
+    }
+}
