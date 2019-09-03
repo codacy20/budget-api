381acc4f99cab2d0b57317627eccb957c8cf0991
codacy20 <kmuc72@gmail.com>
2019-09-03 19:12:23 +0200

    [...]

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerMiddleware } from './logger.middleware';
-import { TimesheetController } from './timesheet/timesheet.controller';
-import { TimesheetService } from './timesheet/timesheet.service';
 import { TimesheetModule } from './timesheet/timesheet.module';
 import { ExpenseService } from './expense/expense.service';
 
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -6,41 +6,41 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class ExpenseService {
+  constructor(
+    @InjectModel('Expense') private readonly expenseModel: Model<Expense>,
+  ) {}
 
-    async findByDate(date: string): Promise<Expense[]> {
-        const dateIso = new Date(date);
-        const year = dateIso.getFullYear();
-        const month = dateIso.getMonth() + 1;
-        const monthPlusOne = dateIso.getMonth() + 2;
-        const obj: Expense[] = await this.expenseModel.find(
-            {
-                date:
-                {
-                    $gte: new Date(year + '-' + month),
-                    $lte: new Date(year + '-' + monthPlusOne),
-                },
-            },
-        ).exec();
-        return obj;
+  async findByDate(date: string): Promise<Expense[]> {
+    const dateIso = new Date(date);
+    const year = dateIso.getFullYear();
+    const month = dateIso.getMonth() + 1;
+    const monthPlusOne = dateIso.getMonth() + 2;
+    const obj: Expense[] = await this.expenseModel
+      .find({
+        date: {
+          $gte: new Date(year + '-' + month),
+          $lte: new Date(year + '-' + monthPlusOne),
+        },
+      })
+      .exec();
+    return obj;
+  }
+
+  async create(expense: CreateExpenseDto): Promise<Expense> {
+    const createdExpense = new this.expenseModel(expense);
+    await createdExpense.save();
+    return createdExpense;
+  }
+
+  public async findAll(): Promise<Expense[]> {
+    return await this.expenseModel.find().exec();
+  }
+
+  async remove(id: string): Promise<Expense> {
+    const obj: Expense = await this.expenseModel.findById({ _id: id }).exec();
+    if (obj) {
+      await this.expenseModel.findByIdAndRemove({ _id: id });
+      return obj;
     }
-
-    constructor(@InjectModel('Expense') private readonly expenseModel: Model<Expense>) { }
-
-    async create(expense: CreateExpenseDto): Promise<Expense> {
-        const createdExpense = new this.expenseModel(expense);
-        await createdExpense.save();
-        return createdExpense;
-    }
-
-    public async findAll(): Promise<Expense[]> {
-        return await this.expenseModel.find().exec();
-    }
-
-    async remove(id: string): Promise<Expense> {
-        const obj: Expense = await this.expenseModel.findById({ _id: id }).exec();
-        if (obj) {
-            await this.expenseModel.findByIdAndRemove({ _id: id });
-            return obj;
-        }
-    }
+  }
 }
