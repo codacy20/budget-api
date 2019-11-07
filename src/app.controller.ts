d2da3c7e8c6b8d6a24def14bc4e304c5af660b66
codacy20 <kmuc72@gmail.com>
2019-11-07 07:55:34 +0100

    init jwt auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RolesGuard } from './roles.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseGuards(RolesGuard)
   getHello(): string {
     return this.appService.getHello();
   }
